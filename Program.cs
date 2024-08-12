22fc0f5fcbbeabf4b16f420adea66e0a7794e0d3
Francis Cottrell-Eshaghi <cottrell-eshaghi@protonmail.com>
2024-08-12 16:50:46 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+static int triple(int x) => x * 3;
+var range = Enumerable.Range(1, 3);
+var triples = range.Select(triple);
+
+Console.WriteLine(string.Join(", ", triples)); // Displaying the values in the triples collection
+Console.WriteLine("asdfasdfasd");
